39f7a2cb1b66f91e6f1c9316c446de928be4511a
sg495 <sg495@users.noreply.github.com>
2019-09-02 13:24:21 +0100

    version 0.0.1 uploaded to PyPI

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sg495/typing-json",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["test"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
--- typing_json/__init__.py
+++ typing_json/__init__.py
@@ -2,5 +2,6 @@
 
 from .typechecking import is_typecheckable, is_instance, is_namedtuple
 from .encoding import is_json_encodable, to_json_obj
+from .decoding import from_json_obj
 
 name: str = "typing_json"
