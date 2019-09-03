b9b35b27e84b5308038be3b618e072225743edb1
sg495 <sg495@users.noreply.github.com>
2019-09-04 00:23:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="typing-json",
-    version="0.0.5",
+    version="0.0.6",
     author="sg495",
     author_email="sg495@users.noreply.github.com",
     description="Add typing support to python JSON serialization.",
@@ -15,10 +15,13 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/sg495/typing-json",
     packages=setuptools.find_packages(exclude=["test"]),
-    classifiers=[
-        "Programming Language :: Python :: 3",
+    classifiers=[ # see https://pypi.org/classifiers/
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Natural Language :: English",
+        "Typing :: Typed",
     ],
     package_data={"": ["LICENSE", "README.md"],
                   "typing_json": ["typing_json/py.typed"],
